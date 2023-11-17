9150a54d53c7d8131ad11e421ddd85f1326d6bab
Andy Watkins <andy.n.watkins@gmail.com>
2023-11-17 22:12:31 +0000

    even more improvements to setup_awcm.sh

--- bits_box/setup_awcm.sh
+++ bits_box/setup_awcm.sh
@@ -6,25 +6,37 @@
 #
 
 
-# Check we've got the tools we need
+echo "------------------------------------------"
+echo "AWCM: Checking we've got the tools we need"
+echo "------------------------------------------"
 # To install these on Ubuntu: sudo apt-get install -y python3-venv git make
-git --version || echo "git is not installed"; exit 1
-python3 --version || echo "python3 is not installed"; exit 1
-make --version || echo "make is not installled"; exit 1
+git --version || exit 1
+python3 --version || exit 1
+make --version || exit 1
 
 
-# Download the static content generator
+
+echo ""
+echo "----------------------------------------------"
+echo "AWCM: Downloading the static content generator"
+echo "----------------------------------------------"
 [[ -e tools ]] || mkdir tools
 cd tools || exit 1
 SRC_DIR=$(pwd)/static-content-generator/
 if [[ ! -d ${SRC_DIR} ]] ; then
     git clone --depth=1 https://github.com/andywis/static-content-generator.git
     rm -rf "${SRC_DIR}/.git"
+else
+    echo "Already downloaded to tools/static-content-generator"
 fi
 cd ..
 
 
-# Create the Python virtualenv
+
+echo ""
+echo "------------------------------------"
+echo "AWCM: Creating the Python virtualenv"
+echo "------------------------------------"
 if [[ ! -d tools/venv ]] ; then
     (
         cd tools || exit 1
@@ -33,11 +45,22 @@ if [[ ! -d tools/venv ]] ; then
         cd "${SRC_DIR}" || exit 1
         # N.B. this installs LXML; it miight be slow
         pip install -r requirements.txt
+
+        echo "AWCM: TEMPORARY FIX... move bits_box out of the way for now."
+        mv bits_box ../
         pip install -e .
+        mv ../bits_box .
     )
+else
+    echo "venv already created in tools/venv"
 fi
 
 
+
+echo ""
+echo "--------------------------"
+echo "AWCM: Installing the tools"
+echo "--------------------------"
 # Make the folders
 [[ -e components ]] || mkdir components
 [[ -e content ]] || mkdir content
@@ -45,8 +68,7 @@ fi
 [[ -e themes ]] || mkdir themes
 [[ -e output ]] || mkdir  output
 
-
-# Install the tools
+# and copy the tool
 cp "${SRC_DIR}"/bits_box/Makefile ./
 cp "${SRC_DIR}"/bits_box/[09]* ./components/
 
@@ -55,3 +77,9 @@ cp "${SRC_DIR}"/bits_box/[09]* ./components/
 # if [[ ! -d themes/default_theme ]] ; then
 #     cp -r path/to/a/theme/default_theme ./themes/
 # fi
+
+echo ""
+echo "---------------------------------------"
+echo "AWCM  is installed and ready to be used"
+echo "---------------------------------------"
+echo "      Type  make  to build your website"
